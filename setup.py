8f8599ba32833bf8a6a55ea6b7db519e9f081e2a
Refael Y <rafi.yzgeav@gmail.com>
2020-11-22 06:40:14 +0000

    add travis CI & tox & setup files

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="scrapy-requests",  # Replace with your own username
+    version="0.1.0",
+    author="Refael Yzg",
+    author_email="rafi.yzgeav@gmail.com",
+    description="Scrapy with requests-html",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rafyzg/scrapy-requests",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'scrapy>=1.0.0',
+        'requests-html>=0.10.0',
+        'codecov',
+        'pytest',
+        'pytest-cov'
+    ]
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        'Intended Audience :: Developers',
+        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    python_requires='>=3.6',
+)
