b658147f433c47571349fa415e09c6897f928dec
rafyzg <rafi.yzgeav@gmail.com>
2021-03-05 08:34:02 -0500

    Add async support for middleware

--- scrapy_requests/middleware.py
+++ scrapy_requests/middleware.py
@@ -1,7 +1,7 @@
 import logging
 from typing import Union
 
-from requests_html import HTMLSession
+from requests_html import AsyncHTMLSession
 from scrapy import signals
 from scrapy.http import HtmlResponse
 from scrapy.spiders import Spider
@@ -15,8 +15,8 @@ class RequestsMiddleware:
     """Scrapy middleware handling requests using requests-html library"""
 
     # Disabling pyppeteer logs to stdout
-    logging.getLogger('websockets').setLevel(20)
-    logging.getLogger('pyppeteer').setLevel(20)
+    logging.getLogger("websockets").setLevel(20)
+    logging.getLogger("pyppeteer").setLevel(20)
 
     def __init__(self):
         self.session = None
@@ -25,45 +25,39 @@ class RequestsMiddleware:
     def from_crawler(cls, crawler):
         """Initialize middleware"""
         middleware = cls()
-        crawler.signals.connect(middleware.spider_opened,
-                                signal=signals.spider_opened)
+        crawler.signals.connect(middleware.spider_opened, signal=signals.spider_opened)
+        crawler.signals.connect(middleware.spider_closed, signal=signals.spider_closed)
         return middleware
 
     def spider_opened(self, spider: Spider) -> None:
         """Open HTMLSession when spider starts"""
-        self.session = HTMLSession()
+        self.session = AsyncHTMLSession()
 
-    def spider_closed(self, spider: Spider) -> None:
-        self.session.close()
+    async def spider_closed(self, spider: Spider) -> None:
+        await self.session.close()
 
-    def process_request(self,
-                        request,
-                        spider: Spider) -> Union[None, HtmlResponse]:
+    async def process_request(
+        self, request, spider: Spider
+    ) -> Union[None, HtmlResponse]:
         """Process a request using requests-html if applicable"""
 
         if not isinstance(request, HtmlRequest):
             return None
 
-        page = self.session.get(request.url)
+        page = await self.session.get(request.url)
 
-        request.meta.update({'page': page})
+        request.meta.update({"page": page})
 
-        render = getattr(request, 'render', False)
+        render = getattr(request, "render", False)
         if not render:
             return HtmlResponse(
-                url=request.url,
-                body=page.html.html,
-                request=request,
-                encoding='utf-8'
+                url=request.url, body=page.html.html, request=request, encoding="utf-8"
             )
 
-        params = getattr(request, 'options', dict())
+        params = getattr(request, "options", dict())
 
-        page.html.render(**params)
+        await page.html.arender(**params)
 
         return HtmlResponse(
-            url=request.url,
-            body=page.html.html,
-            request=request,
-            encoding='utf-8'
+            url=request.url, body=page.html.html, request=request, encoding="utf-8"
         )
