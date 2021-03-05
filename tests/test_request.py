2e5b227954e9df8df7060332e23f5a8e8d5798eb
rafyzg <rafi.yzgeav@gmail.com>
2021-03-05 08:56:10 -0500

    Migrate test code style to Black

--- tests/test_request.py
+++ tests/test_request.py
@@ -10,32 +10,40 @@ from scrapy.utils.test import get_crawler
 from scrapy_requests import HtmlRequest, RequestsMiddleware
 
 # Installing Asyncio Selector
-install_reactor('twisted.internet.asyncioreactor.AsyncioSelectorReactor')
+install_reactor("twisted.internet.asyncioreactor.AsyncioSelectorReactor")
+
 
 @contextmanager
 def opened_middleware(crawler):
     mw = RequestsMiddleware.from_crawler(crawler)
-    crawler.spider = crawler._create_spider('example')
+    crawler.spider = crawler._create_spider("example")
     mw.spider_opened(crawler.spider)
     yield mw
+
 
 def test_configured():
     crawler = get_crawler()
     mw = RequestsMiddleware.from_crawler(crawler)
-    assert mw.session is None 
+    assert mw.session is None
+
 
 @pytest.mark.asyncio
 async def test_crawl():
-    nest_asyncio.apply() # Required for async tests
-    crawler = get_crawler(settings_dict={'TWISTED_REACTOR' : 'twisted.internet.asyncioreactor.AsyncioSelectorReactor'})
-    req1 = scrapy.Request(url='https://pythonclock.org/')
-    req2 = HtmlRequest(url='https://pythonclock.org/', render=True)
-    req3 = HtmlRequest(url='https://pythonclock.org/', render=True, options={
-                       'sleep': 1, 'timeout': 12})
+    nest_asyncio.apply()  # Required for async tests
+    crawler = get_crawler(
+        settings_dict={
+            "TWISTED_REACTOR": "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
+        }
+    )
+    req1 = scrapy.Request(url="https://pythonclock.org/")
+    req2 = HtmlRequest(url="https://pythonclock.org/", render=True)
+    req3 = HtmlRequest(
+        url="https://pythonclock.org/", render=True, options={"sleep": 1, "timeout": 12}
+    )
 
     with opened_middleware(crawler) as mw:
         assert mw.session is not None
-        output = scrapy.Request('https://pythonclock.org/')
+        output = scrapy.Request("https://pythonclock.org/")
 
         # No HtmlRequest
         res = await mw.process_request(req1, crawler.spider)
@@ -48,6 +56,6 @@ async def test_crawl():
         # Render is True and assigned options
         res = await mw.process_request(req3, crawler.spider)
         count = res.xpath("//span[@class='countdown-amount']/text()").extract()[0]
-        assert count == '0'
-        
+        assert count == "0"
+
         await mw.spider_closed(crawler.spider)
